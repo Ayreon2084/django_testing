56e0c6489ead632672f5fdd6c117473e285551d6
Alexander Zhukov <alexander.zhukov0709@gmail.com>
2024-05-31 18:16:18 +0500

    Edit ya_news pytest test_logic

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from random import choice
 
 from django.urls import reverse
 import pytest
@@ -21,10 +22,10 @@ def test_unauthorized_user_cant_create_comment(
 
 
 def test_authorized_user_can_create_comment(
-        auth_user,
-        auth_user_client,
-        news,
-        post_comment
+    auth_user,
+    auth_user_client,
+    news,
+    post_comment
 ):
     url = reverse('news:detail', args=(news.id,))
     auth_user_client.post(url, data=post_comment)
@@ -42,7 +43,7 @@ def test_user_cant_use_bad_words(
 ):
     url = reverse('news:detail', args=(news.id,))
     comment_with_bad_words = {
-        'text': f'Tests is the most {BAD_WORDS[0]} boring topic!'
+        'text': f'Tests is the most {choice(BAD_WORDS)} boring topic!'
     }
     response = auth_user_client.post(url, data=comment_with_bad_words)
     assertFormError(
@@ -69,13 +70,25 @@ def test_author_can_delete_comment(
     assert comments_count == 0
 
 
-def test_authorized_user_cant_delete_comment_of_another_user(
+def test_user_cant_delete_comment_of_another_user(
         auth_user_client,
         comment
 ):
     delete_comment_url = reverse('news:delete', args=(comment.id,))
     response = auth_user_client.delete(delete_comment_url)
     assert response.status_code == HTTPStatus.NOT_FOUND
+    comments_count = Comment.objects.count()
+    assert comments_count == 1
+
+
+@pytest.mark.django_db
+def test_unauthorized_user_cant_delete_comment(
+    client,
+    comment
+):
+    delete_comment_url = reverse('news:delete', args=(comment.id,))
+    response = client.delete(delete_comment_url)
+    assert response.status_code == HTTPStatus.FOUND
     comments_count = Comment.objects.count()
     assert comments_count == 1
 
@@ -95,7 +108,7 @@ def test_author_can_edit_comment(
     assert comment.text == post_comment['text']
 
 
-def test_authorized_user_cant_edit_comment_of_another_user(
+def test_user_cant_edit_comment_of_another_user(
         auth_user_client,
         comment,
         post_comment
@@ -104,5 +117,19 @@ def test_authorized_user_cant_edit_comment_of_another_user(
     current_comment_text = comment.text
     response = auth_user_client.post(edit_comment_url, data=post_comment)
     assert response.status_code == HTTPStatus.NOT_FOUND
+    comment.refresh_from_db()
+    assert comment.text == current_comment_text
+
+
+@pytest.mark.django_db
+def test_unauthorized_user_cant_edit_comment(
+        client,
+        comment,
+        post_comment
+):
+    edit_comment_url = reverse('news:edit', args=(comment.id,))
+    current_comment_text = comment.text
+    response = client.post(edit_comment_url, data=post_comment)
+    assert response.status_code == HTTPStatus.FOUND
     comment.refresh_from_db()
     assert comment.text == current_comment_text
