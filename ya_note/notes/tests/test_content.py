c8282ac8af3f4dd8dbccb22585f6c2f55ae053c6
Alexander Zhukov <alexander.zhukov0709@gmail.com>
2024-06-01 21:26:59 +0500

    Edit ya_note unittest content (clients to setUp, test statuses)

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
@@ -11,17 +13,20 @@ User = get_user_model()
 class TestContent(TestCase):
 
     @classmethod
+    def setUp(cls):
+        cls.author_client = Client()
+        cls.author_client.force_login(cls.author)
+        cls.auth_user_client = Client()
+        cls.auth_user_client.force_login(cls.auth_user)
+
+    @classmethod
     def setUpTestData(cls):
         cls.author = User.objects.create(
             username='Author',
         )
-        cls.author_client = Client()
-        cls.author_client.force_login(cls.author)
         cls.auth_user = User.objects.create(
             username='Random user',
         )
-        cls.auth_user_client = Client()
-        cls.auth_user_client.force_login(cls.auth_user)
         cls.note = Note.objects.create(
             title='Title',
             text='Text',
@@ -37,17 +42,20 @@ class TestContent(TestCase):
         for name, kwargs in urls:
             with self.subTest(name=name):
                 response = self.client.get(name, kwargs=kwargs)
+                self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
                 self.assertNotIn('form', response.context)
 
     def test_create_note_form_available_for_authorized_users(self):
         url = reverse('notes:add')
         response = self.auth_user_client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertIn('form', response.context)
         self.assertIsInstance(response.context['form'], NoteForm)
 
     def test_edit_note_form_available_for_author(self):
         url = reverse('notes:edit', kwargs={'slug': self.note.slug})
         response = self.author_client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertIn('note', response.context)
         self.assertIn('form', response.context)
         self.assertIsInstance(response.context['form'], NoteForm)
@@ -55,11 +63,13 @@ class TestContent(TestCase):
     def test_note_in_list_for_author(self):
         url = reverse('notes:list')
         response = self.author_client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         object_list = response.context['object_list']
         assert self.note in object_list
 
     def test_note_not_in_list_for_authorized_user(self):
         url = reverse('notes:list')
         response = self.auth_user_client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         object_list = response.context['object_list']
         assert self.note not in object_list
